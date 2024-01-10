5e2db8d0861881d9bd3aefe13cb58b33ffa17b4f
Nitish <nitishneeraj16@gmail.com>
2024-01-11 00:49:47 +0530

    Created new model file (GST bill) and wrote GST and partial relation function

---
+++ app/Models/GstBill.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class GstBill extends Model
+{
+    use HasFactory;
+    protected $table = 'gst_bills';
+    protected $fillable = [
+        'party_id', 'invoice_date',
+        'invoice_no', 'item_description',
+        'total_amount', 'cgst_rate',
+        'sgst_rate', 'igst_rate',
+        'cgst_amount', 'sgst_amount',
+        'igst_amount', 'tax_amount',
+        'net_amount', 'declaration'
+    ];
+    public function party()
+    {
+        return $this->belongsTo(Party::class);
+    }
+}
