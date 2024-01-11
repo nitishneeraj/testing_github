342b469f2f9b6060c7597be79051a3d7fbf3e257
Nitish <nitishneeraj16@gmail.com>
2024-01-12 00:23:24 +0530

    added some route venderinvoice

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AppController;
 use App\Http\Controllers\GstBillController;
 use App\Http\Controllers\PartyController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VendorInvoiceController;
 
 
 //Gst Billing
@@ -23,3 +24,18 @@ Route::get('/add-gst-bill', [GstBillController::class, 'addGstBill'])->name('add
 Route::get('/manage-gst-bills', [GstBillController::class, 'index'])->name('manage-gst-bills');
 Route::get('/print-gst-bill/{id}', [GstBillController::class, 'print'])->name('print-gst-bill');
 Route::post('/create-gst-bill', [GstBillController::class, 'creategstBill'])->name('create-gst-bill');
+
+//Soft delete route
+Route::get('/delete/{table}/{id}', [AppController::class, 'delete'])->name('delete');
+
+// Resource Controller routes
+Route::resource('vendor-invoice', 'VendorInvoiceController');
+//Route::resource('vendor-invoice', 'VendorInvoice');
+
+Route::get('/vendor-invoice', [VendorInvoiceController::class, 'create'])->name('create-vendor');
+Route::post('/vendor-invoice', [VendorInvoiceController::class, 'store'])->name('vendor-invoice.store');
+Route::get('/vendor-invoic/{id}', [VendorInvoiceController::class, 'show'])->name('vendor-invoice.show');
+
+// Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
